06852dff452d2d696a8783b74dd64f7aba74152c
Elvis Blessing Eunice <amadinelvis6@gmail.com>
2024-02-08 16:57:42 +0000

    Correction errors

--- assets/github-information.js
+++ assets/github-information.js
@@ -1,14 +1,14 @@
 function fetchGitHubInformation(event) {
 
-    var username = $("#gh-username").val(); //The val() get the value from the test field which the user input
+    var username = $("#gh-username").val(); //The val() get the value from the test field which is the user input
      
-    //Return a piece of text if the if the username field is empty
+    //Display a piece of text if the if the username field is empty
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
         return;
     }
     /**
-     * if text has been inputted into the field the display
+     * if text is been inputted into the field display
      * an amimated loader
      */
     $("#gh-user-data").html(
